c3058bb6a6d50674c2e3a5a3eec2df290bb02a94
timmalstead <timmalstead@gmail.com>
2022-12-07 16:32:34 -0800

    seventh day refactor

--- day7/index.ts
+++ day7/index.ts
@@ -2,58 +2,98 @@
 import {loadInput, newLine, sum} from '../utils'
 const input = loadInput(__dirname).split(newLine)
 
-const [fileStructure, directorySizes] = [{'/': {}}, {'/': 0}]
-let currentDir = "fileStructure['/']"
+type FileSizes = {'/': number; [dir: string]: number}
+type DriveStructure = {'/': {[dir: string]: any}}
 
-for (const line of input) {
-    const isCommand = line.startsWith('$')
-    if (isCommand) {
-        if (line.includes('cd')) {
-            const directory = line.split(' ').pop()
+const moveUpOneDir = (str: string, delimiter: string): string =>
+    str.slice(0, str.lastIndexOf(delimiter))
 
-            if (directory === '/') continue
-            else if (directory === '..') {
-                const splitDirectories = currentDir.split('[')
-                splitDirectories.pop()
-                currentDir = splitDirectories.join('[')
-            } else currentDir += `['${directory}']`
-        } else if (line.includes('ls')) continue
-    } else {
-        if (line.startsWith('dir')) {
-            const dirToCreate = line.split(' ').pop()
-            const createDirectoryCommand = `${currentDir}['${dirToCreate}'] = {}`
-            eval(createDirectoryCommand)
+const createFileStructure = (input: string[]): DriveStructure => {
+    const drive: DriveStructure = {'/': {}}
+    let currentDir = "drive['/']"
+
+    for (const line of input) {
+        const isCommand = line.startsWith('$')
+        if (isCommand) {
+            const isDirCommand = line.includes('cd')
+            if (isDirCommand) {
+                const directory = line.split(' ').pop()
+                const [isRoot, movingUpOneDir] = [
+                    directory === '/',
+                    directory === '..',
+                ]
+                const movingToLowerDir = !isRoot && !movingUpOneDir
+
+                if (isRoot) continue
+                else if (movingUpOneDir)
+                    currentDir = moveUpOneDir(currentDir, '[')
+                else if (movingToLowerDir) currentDir += `['${directory}']`
+            }
         } else {
-            const [size, name] = line.split(' ')
-            const sizeAsNumber = +size
-            const createFileCommand = `${currentDir}['${name}'] = ${sizeAsNumber}`
-            eval(createFileCommand)
-
-            const strippedDir = currentDir
-                .replace("fileStructure['", '')
-                .replace(/\[\'/g, ',')
-                .replace(/\'\]/g, '')
-
-            let dirToUpdate = strippedDir
-            while (dirToUpdate) {
-                if (dirToUpdate in directorySizes)
-                    directorySizes[dirToUpdate] += sizeAsNumber
-                else directorySizes[dirToUpdate] = sizeAsNumber
-                const dirAsArr = dirToUpdate.split(',')
-                dirAsArr.pop()
-                dirToUpdate = dirAsArr.join(',')
+            const isDirOutput = line.startsWith('dir')
+            if (isDirOutput) {
+                const dirToCreate = line.split(' ').pop()
+                const createDirectoryCommand = `${currentDir}['${dirToCreate}'] = {}`
+                eval(createDirectoryCommand)
+            } else {
+                const [size, name] = line.split(' ')
+                const createFileCommand = `${currentDir}['${name}'] = ${size}`
+                eval(createFileCommand)
             }
         }
     }
+
+    return drive
 }
 
-const entries = Object.values(directorySizes)
+const indexFileSizes = (input: string[]): FileSizes => {
+    const sizes: FileSizes = {'/': 0}
+    let currentDir = '/'
 
-const smallDirs = entries.filter((n) => n <= 100000)
-// very tired when i wrote below callback and i will admit to copying it from https://github.com/leyanlo/advent-of-code/blob/581c079918284d88bdcb9aec5db4b7e889695f78/2022/day-07.js#L41
-const largeDirs = entries.filter((n) => n >= directorySizes['/'] - 40000000)
+    for (const line of input) {
+        const isCommand = line.startsWith('$')
+        if (isCommand) {
+            const isChangingDirectory = line.includes('cd')
+            if (isChangingDirectory) {
+                const directory = line.split(' ').pop()
+                const [isRoot, movingUpOneDir] = [
+                    directory === '/',
+                    directory === '..',
+                ]
+                const movingToLowerDir = !isRoot && !movingUpOneDir
+
+                if (isRoot) continue
+                else if (movingUpOneDir)
+                    currentDir = moveUpOneDir(currentDir, ',')
+                else if (movingToLowerDir) currentDir += `,${directory}`
+            }
+        } else {
+            const isFile = !line.startsWith('dir')
+            if (isFile) {
+                const size = +line.split(' ').shift()
+
+                let dirToUpdate = currentDir
+                while (dirToUpdate) {
+                    if (dirToUpdate in sizes) sizes[dirToUpdate] += size
+                    else sizes[dirToUpdate] = size
+
+                    dirToUpdate = moveUpOneDir(dirToUpdate, ',')
+                }
+            }
+        }
+    }
+
+    return sizes
+}
+
+const indexSizes = indexFileSizes(input)
+const values = Object.values(indexSizes)
+
+const smallDirs = values.filter((n) => n <= 100000)
+const largeDirs = values.filter((n) => n >= indexSizes['/'] - 40000000)
 
 const firstPartFinalSum = smallDirs.reduce(sum, 0)
 const secondPartFinalSum = Math.min(...largeDirs)
 console.log(firstPartFinalSum)
 console.log(secondPartFinalSum)
+console.log(createFileStructure(input))
